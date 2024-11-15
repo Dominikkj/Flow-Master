c41e1b4fa17801b08a4e512b1ab5d38a68edb2c6
Santinho <114532776+SantinhoAcari@users.noreply.github.com>
2024-11-14 21:06:23 -0300

    tentando fazer um slide
    tentando fazer um slide,

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-const menu = document.querySelector('.menu');
-const NavMenu = document.querySelector('.nav-menu')
+// let slideIndex = 0;
+// showSlides(slideIndex);
 
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo')
-})
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+
+// } function showSlides(n) {
+//     let slides = document.getElementsByClassName("slides");
+//     if (n >= slides.length) { slideIndex = 0; }
+//     if (n < 0) { slideIndex = slides.length - 1; }
+
+//     for (let i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+
+//     slides[slideIndex].style.display = "block";
+// }
+
+const carrossel = document.querySelector(".carrossel");
+
+let isDragStart = false;
+const dragStart = () => {
+    isDragStart = true;
+}
+const puxar = (e) => {
+    if(!isDragStart) return;
+    carrossel.scrollLeft = e.pageX}
+ 
+    carrossel.addEventListener("mousemove", dragStart);
+    carrossel.addEventListener("mousemove", puxar);
